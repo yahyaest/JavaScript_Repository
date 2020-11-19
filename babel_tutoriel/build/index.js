097ce2cda95dd7e4932033f9d95832022a4b67c4
Yahya Machat <machatyahya@gmail.com>
2020-11-19 22:14:58 +0100

    JavaScript Repository: 19/11/2020

---
+++ babel_tutoriel/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var x = 1;
+
+var data = [4, 5, 6];
+
+var result = data.forEach(function (input) {
+  return console.log(input * input);
+});
---
+++ babel_tutoriel/index.js
@@ -0,0 +1,5 @@
+const x = 1;
+
+const data = [4, 5, 6];
+
+const result = data.forEach((input) => console.log(input * input));
